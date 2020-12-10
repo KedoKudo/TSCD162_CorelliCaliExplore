5821367438d986ba3bd570e967cf6958c13760dd
Chen Zhang <zhangc@ornl.gov>
2020-12-10 14:20:36 -0500

    add testing script

---
+++ test.py
@@ -0,0 +1,19 @@
+# import mantid algorithms, numpy and matplotlib
+from mantid.simpleapi import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+# load and merge the peaks
+LoadIsawPeaks(Filename='/SNS/CORELLI/IPTS-23019/shared/Natrolite/Natrolite_runs_133752_133812.peaks', OutputWorkspace='pws1')
+LoadIsawPeaks(Filename="/SNS/CORELLI/IPTS-23019/shared/Natrolite/Natrolite_runs_133813_133871.peaks", OutputWorkspace="pws2")
+CombinePeaksWorkspaces(LHSWorkspace="pws1", RHSWorkspace="pws2", OutputWorkspace="pws")
+
+# calibrate the detector with SCDCalibratePanels
+SCDCalibratePanels(
+    PeakWorkspace='pws',
+    DetCalFilename='corelli_natrolite.DetCal',
+    XmlFilename='corelli_natrolite.xml',
+    a=18.29, b=18.64, c=6.56, 
+    alpha=90, beta=90, gamma=90,
+    )
+
