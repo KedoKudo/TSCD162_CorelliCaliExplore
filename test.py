40e03238b6997ef3e8e950aa141a94b5f6ce7b3e
Chen Zhang <zhangc@ornl.gov>
2020-12-11 09:36:56 -0500

    test impact of different options with real data

--- test.py
+++ test.py
@@ -3,21 +3,41 @@ from mantid.simpleapi import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+# ------------------------
 # load and merge the peaks
 LoadIsawPeaks(Filename='/SNS/CORELLI/IPTS-23019/shared/Natrolite/Natrolite_runs_133752_133812.peaks', OutputWorkspace='pws1')
 LoadIsawPeaks(Filename="/SNS/CORELLI/IPTS-23019/shared/Natrolite/Natrolite_runs_133813_133871.peaks", OutputWorkspace="pws2")
 CombinePeaksWorkspaces(LHSWorkspace="pws1", RHSWorkspace="pws2", OutputWorkspace="pws")
 
-# calibrate the detector with SCDCalibratePanels
+# ----------------------------
+# load the associated nxs file
+filename = "/SNS/CORELLI/IPTS-16227/nexus/CORELLI_23019.nxs.h5"
+LoadEventNexus(Filename=filename, OutputWorkspace='ws')
+
+# ----------------------------------------------
+# calibrate the detector with SCDCalibratePanels 
+# without adjusting L1 and T0
+SCDCalibratePanels(
+    PeakWorkspace='pws',
+    DetCalFilename='corelli_natrolite_noL1T0.DetCal',
+    XmlFilename='corelli_natrolite_noL1T0.xml',
+    a=18.29, b=18.64, c=6.56, 
+    alpha=90, beta=90, gamma=90,
+    ChangeL1=False, ChangeT0=False,
+    )
+CloneWorkspace(InputWorkspace="ws", OutputWorkspace="ws_noL1T0")
+LoadParameterFile(Workspace="ws_noL1T0", Filename='corelli_natrolite_noL1T0.xml')
+
+# ----------------------------------------------
+# calibrate the detector with SCDCalibratePanels 
+# with adjusting L1 and T0
 SCDCalibratePanels(
     PeakWorkspace='pws',
     DetCalFilename='corelli_natrolite.DetCal',
     XmlFilename='corelli_natrolite.xml',
     a=18.29, b=18.64, c=6.56, 
     alpha=90, beta=90, gamma=90,
+    ChangeL1=True, ChangeT0=True,
     )
-
-# load empty instrument
-LoadEmptyInstrument(Filename="CORELLI_Definition.xml", OutputWorkspace="cws")
-LoadParameterFile(Workspace="cws", Filename='corelli_natrolite.xml')
-
+CloneWorkspace(InputWorkspace="ws", OutputWorkspace="ws_fitAll")
+LoadParameterFile(Workspace="ws_fitAll", Filename='corelli_natrolite.xml')
